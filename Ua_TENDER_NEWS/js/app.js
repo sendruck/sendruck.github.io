edf61e4cd7a931be340766cd3e4131ad14f8a2e4
sendruck <olexandr.yakov@gmail.com>
2018-02-06 16:34:47 +0200

    changes

--- Ua_TENDER_NEWS/js/app.js
+++ Ua_TENDER_NEWS/js/app.js
@@ -7,27 +7,28 @@ $(document).ready(function () {
     });
 
     checkScreenSize();
-    fullName = null;
 
     function checkScreenSize(){
         var newWindowWidth = $(window).width();
         if (newWindowWidth < 480) {
-            $(".news_info_wrapper p").each(function( index ) {
+            $(".news_info_block p").each(function( index ) {
                 if ($(this).text().length > 150) {
                     var fullName = $(this).text(),
                         shortName = $(this).text().slice(0, 150),
                         dots_btn = $("<span class='dotsBtn'>...</span>");
                     $(this).text(shortName).append(dots_btn);
-
+                
                     $(".dotsBtn").on('click', function (event)  {
                         event.preventDefault();
                         $(this).parent().text(fullName);
                     });
                 } else {
-                    $(".news_info_wrapper p").text(fullName);
+                    $(".news_info_block p").text(fullName);
                     $(".dotsBtn").remove;
-                }
-            });    
+                };
+            });
+
+            
         }   
     }
 
