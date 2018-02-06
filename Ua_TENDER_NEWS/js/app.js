8e841a6ec65a4d10dc85c2864d21bb07514d2ea3
sendruck <olexandr.yakov@gmail.com>
2018-02-06 16:14:02 +0200

    fixes

--- Ua_TENDER_NEWS/js/app.js
+++ Ua_TENDER_NEWS/js/app.js
@@ -18,16 +18,15 @@ $(document).ready(function () {
                         shortName = $(this).text().slice(0, 150),
                         dots_btn = $("<span class='dotsBtn'>...</span>");
                     $(this).text(shortName).append(dots_btn);
-                
+
+                    $(".dotsBtn").on('click', function (event)  {
+                        event.preventDefault();
+                        $(this).parent().text(fullName);
+                    });
                 } else {
                     $(".news_info_wrapper p").text(fullName);
                     $(".dotsBtn").remove;
-                };
-
-                $(".dotsBtn").on('click', function (event)  {
-                    event.preventDefault();
-                    $(this).parent().text(fullName);
-                });
+                }
             });    
         }   
     }
