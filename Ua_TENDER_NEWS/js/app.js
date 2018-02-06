481db5af0ab9cb0f7a8a1879c30512390e3d02b5
sendruck <olexandr.yakov@gmail.com>
2018-02-06 12:55:40 +0200

    added js for shortenning news articles

--- Ua_TENDER_NEWS/js/app.js
+++ Ua_TENDER_NEWS/js/app.js
@@ -1,9 +1,6 @@
 $(document).foundation();
-
-// Cut long title
-$(document).ready(function () {  
-
-
+$(document).ready(function () {
+    // SHORTENT LONG TENDER LINK
     $(window).on("load", function (e) {
         checkScreenSize();
     });
@@ -11,24 +8,26 @@ $(document).ready(function () {
     checkScreenSize();
 
     function checkScreenSize(){
-        var normalSize_HTML = $(".predmet_wrapper").html();
         var newWindowWidth = $(window).width();
         if (newWindowWidth < 480) {
-            if ($(".predmet_zak").text().length > 260) {
-                var fullName = $(".predmet_zak").text(),
-                    shortName = $(".predmet_zak").text().slice(46, 260),
-                    dots_btn = $("<span class='dotsBtn'>...</span>");
-                $(".predmet_zak").text(shortName).append(dots_btn);
-                $(".predmet_zak").prepend('<span class="noHighlight">Предмет закупки:</span>');
-            
-                $(".dotsBtn").on('click', function () {
-                    $(".predmet_zak").html(normalSize_HTML);
-                });
-            }
-        }
-        else
-        {
-            $(".predmet_zak").html(normalSize_HTML);
-        }
+            $(".news_info_wrapper p").each(function( index ) {
+                if ($(this).text().length > 150) {
+                    var fullName = $(this).text(),
+                        shortName = $(this).text().slice(0, 150),
+                        dots_btn = $("<span class='dotsBtn'>...</span>");
+                        console.log("full Name" + fullName);
+                        console.log("short Name" + shortName);
+                    $(this).text(shortName).append(dots_btn);
+                
+                    $(".dotsBtn").on('click', function (event)  {
+                        event.preventDefault();
+                        $(this).parent().text(fullName);
+                    });
+                } else {
+                    $(".news_info_wrapper p").text(fullName);
+                    $(".dotsBtn").remove;
+                };
+            });    
+        }   
     }
 });
