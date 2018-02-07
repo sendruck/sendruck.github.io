67d60dc78ee15eb4da042f0ca31b27e5dbddab39
sendruck <olexandr.yakov@gmail.com>
2018-02-07 10:37:51 +0200

    wip

--- Ua_TENDER_NEWS/js/app.js
+++ Ua_TENDER_NEWS/js/app.js
@@ -1,32 +1,33 @@
 $(document).foundation();
 $(document).ready(function () {
-    var fullName = null;
+    
 
     // SHORTENT LONG TENDER LINK
     $(window).on("load", function (e) {
         checkScreenSize();
     });
-
     checkScreenSize();
 
     function checkScreenSize(){
         var newWindowWidth = $(window).width();
         if (newWindowWidth < 480) {
             $(".news_info_block p").each(function( index ) {
+                var fullName, shortName;
                 if ($(this).text().length > 150) {
                     console.log("here");
-                    var fullName = $(this).text(),
-                        shortName = $(this).text().slice(0, 151),
-                        dots_btn = $("<span class='dotsBtn'>...</span>");
+                    fullName = $(this).text();
+                    shortName = $(this).text().slice(0, 151);
+                    var dots_btn = $("<span class='dotsBtn'>...</span>");
                     $(this).text(shortName).append(dots_btn);
                 } else {
                     $(".dotsBtn").remove;
                 };
                 $(".dotsBtn").on('click', function (event)  {
                     event.preventDefault();
-                    console.log(fullName);
                     $(this).parent().text(fullName);
                 });
+                console.log("fullName = " + fullName);
+                console.log("shortName = " + shortName);
             });
         }   
     }
