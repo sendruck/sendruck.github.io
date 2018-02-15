c0de2287d0a1010d78f418f638cf8574a13cf7e2
sendruck <olexandr.yakov@gmail.com>
2018-02-15 12:29:10 +0200

    legislation laws mobile page

--- ua_tenderss/dist/assets/js/app.js
+++ ua_tenderss/dist/assets/js/app.js
@@ -22487,16 +22487,18 @@ function NewscheckScreenSize() {
     if (newWindowWidth < 768) {
         $(".pagination_wrapp").addClass("align-center");
         $(".news_info_wrapper p").each(function (index) {
-            if ($(this).text().length > 150) {
-                var fullName = $(this).text(),
-                    shortName = $(this).text().slice(0, 150),
-                    dots_btn = $("<span class='dotsBtn'>...</span>");
-                $(this).text(shortName).append(dots_btn);
+            if ($(this).text().length > 140) {
+                if (!$(this).parent().hasClass("legislation_laws_wrapper")) {
+                    var fullName = $(this).text(),
+                        shortName = $(this).text().slice(0, 140),
+                        dots_btn = $("<span class='dotsBtn'>...</span>");
+                    $(this).text(shortName).append(dots_btn);
 
-                $(".dotsBtn").on('click', function (event) {
-                    event.preventDefault();
-                    $(this).parent().text(fullName);
-                });
+                    $(".dotsBtn").on('click', function (event) {
+                        event.preventDefault();
+                        $(this).parent().text(fullName);
+                    });
+                }
             } else {
                 $(".news_info_wrapper p").text(fullName);
                 $(".dotsBtn").remove;
