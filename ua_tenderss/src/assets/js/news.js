31d43bb37d26a47f9deca2b452b41676f34fd83c
sendruck <olexandr.yakov@gmail.com>
2018-02-28 17:26:04 +0200

    news page tablet version done

--- ua_tenderss/src/assets/js/news.js
+++ ua_tenderss/src/assets/js/news.js
@@ -1,7 +1,7 @@
 // Cut long news text
 export function NewscheckScreenSize(){
     var newWindowWidth = $(window).width();
-    if (newWindowWidth < 768) {
+    if (newWindowWidth < 1024) {
         $(".pagination_wrapp").addClass("align-center");
         $(".news_info_wrapper p").each(function( index ) {
             if ($(this).text().length > 140) {
